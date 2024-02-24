38bb8fa195ae580ef00d924d70cf9ecf70a19ca7
ChiefLopes <chieflopesekede@gmail.com>
2024-02-24 15:41:51 +0100

    added some global stylings

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-  <h1 className='font-bold m-5 text-2xl text-teal-500'>
+  <h1 className='font-bold m-5 text-2xl text-gray-30 '>
     Hello NExt!
   </h1>
   )
