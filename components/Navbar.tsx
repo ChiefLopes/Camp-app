157fbde63adc2e170eeec7e8ef9dddee9810aada
ChiefLopes <chieflopesekede@gmail.com>
2024-02-24 19:34:23 +0100

    added the media files I'd be working with, also added the nav image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container relative padding-container z-30 py-2">
+    <nav className="flexBetween max-container relative padding-container z-30 py-5">
 <Link href="/">
     <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
 </Link>
