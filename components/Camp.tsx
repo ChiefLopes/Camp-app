19260e9ba53e4d9c722f86a643bd1749f28296c0
ChiefLopes <chieflopesekede@gmail.com>
2024-02-25 17:13:40 +0100

    added the svg into the rounded div

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CampProps {
   backgroundImage: string;
   title: string;
@@ -16,7 +18,11 @@ const CampSite = ({
       className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl `}
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
-        <div className="flexCenter gap-4 "></div>
+        <div className="flexCenter gap-4 ">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image src="/folded-map.svg" alt="folded map" width={24}  height={24}/>
+          </div>
+        </div>
       </div>
     </div>
   );
