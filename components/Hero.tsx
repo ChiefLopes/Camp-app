7d2ca0093162d520add4d2a6054e129b0a7fbaa6
ChiefLopes <chieflopesekede@gmail.com>
2024-02-25 02:51:00 +0100

    added the stars for the hero section using the Array(n) method to specify the number of stars needed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,6 +20,16 @@ const Hero = () => {
           seeing the incorruptible beauty of nature. We can help you on an
           adventure around the world in just one app
         </p>
+
+        <div className="my-11 flex flex-wrap gap-5 ">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image src="/star.svg" key={index} alt="stars" width={24} height={24} />
+              ))}
+          </div>
+        </div>
       </div>
     </section>
   );
