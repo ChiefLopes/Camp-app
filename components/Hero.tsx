6a5b4da8764075a5afd2dc92730b1508179fd2d8
ChiefLopes <chieflopesekede@gmail.com>
2024-02-25 02:25:37 +0100

    started working on the Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 border-2 border-red-500"></section>
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-grow border-2 border-red-500">
+      <div className="hero-map" />
+      
+      {/* LEFT SIDE */}
+      <div className="relative z-20 flex flex-1 flex-col">
+
+      </div>
+    </section>
   );
 };
 
