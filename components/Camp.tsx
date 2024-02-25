ee23cf5419b8d6e395c3f1336b593155a19b870b
ChiefLopes <chieflopesekede@gmail.com>
2024-02-25 16:22:50 +0100

    over to the camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const Camp = () => {
-  return (
-    <div>Camp</div>
-  )
-}
+  return <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20 ">
 
-export default Camp
+  </section>;
+};
+
+export default Camp;
