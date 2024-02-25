eb480dfc2c30703a59969e8fdf41be6c65f44ac2
ChiefLopes <chieflopesekede@gmail.com>
2024-02-25 01:52:52 +0100

    added the hamburger icon and set it hidden for large devices while visible on smaller devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,6 +30,8 @@ const Navbar = () => {
           icon="/user.svg"
         />
       </div>
+
+      <Image src="menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden"/>
     </nav>
   );
 };
