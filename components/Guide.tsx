525ffb05df1d00c50c2c2a6ab7ee5bd33e0f6697
ChiefLopes <chieflopesekede@gmail.com>
2024-02-27 07:40:27 +0100

    finished with the Features section and unto the guide section of the app

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -23,7 +23,7 @@ const Guide = () => {
         </div>
       </div>
 
-      <div className="flexCenter max-container w-full relative border-2 border-green-500">
+      <div className="flexCenter max-container w-full relative">
         <Image
           src="/boat.png"
           alt="baot"
@@ -31,7 +31,31 @@ const Guide = () => {
           height={580}
           className="w-full object-cover object-center 2xl:rounded-5xl"
         />
-        <div className="absolute flex bg-white py-8 pl-5 pr-7"></div>
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+            <Image
+              src="meter.svg"
+              alt="meter"
+              width={16}
+              height={158}
+              className="h-full w-auto"
+            />
+          <div className="flexBetween flex-col">
+            <div className="flex flex-col w-full">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Agua Calientas</p>
+            </div>
+
+            <div className="flex flex-col w-full">
+              <p className="regular-16 text-gray-20 mt-2">Start Track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap ">
+                Wonorejo Pasuruan
+              </h4>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
