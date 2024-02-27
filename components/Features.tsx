60040d7f10c893524d5ebae098e23114770ba7f5
ChiefLopes <chieflopesekede@gmail.com>
2024-02-27 07:49:45 +0100

    finished with the guide section of the app

--- components/Features.tsx
+++ components/Features.tsx
@@ -58,7 +58,7 @@ const FeatureItem = ({ title, icon, description }: featureProps) => {
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} className="" />
       </div>
-      <h2 className="bold-16 lg:bold-32 mt-5 capitalize">{title}</h2>
+      <h2 className="bold-16 lg:bold-28 mt-5 capitalize">{title}</h2>
       <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
         {description}
       </p>
