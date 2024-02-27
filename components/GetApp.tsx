93c7d38f0c9f08af839c2c68b23bfea215874e40
ChiefLopes <chieflopesekede@gmail.com>
2024-02-27 08:59:44 +0100

    done the first part of the getApp section

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
-  )
-}
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        {/* LEFT SIDE */}
+        <div className="z-20 flex flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="capitalize bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get for free now
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on iOS and Android, Download now!
+          </p>
 
-export default GetApp
+          <div className="flex w-full">
+            <Button
+              type="button"
+              title="App Store"
+              variant="btn_white"
+              icon="/apple.svg"
+              full
+            />
+            <Button
+              type="button"
+              title="Play Store"
+              variant="btn_dark_green_outline"
+              icon="/android.svg"
+              full
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetApp;
