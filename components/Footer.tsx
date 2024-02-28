38c61390ad3b47116d2ea2ec85a1cf32bce96f85
ChiefLopes <chieflopesekede@gmail.com>
2024-02-28 04:46:08 +0100

    working on the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10]% md:flex-row ">
-          <Link href="/">
+          <Link href="/" className="mb-10">
             <Image
               src="/hilink-logo.svg"
               alt="hilink logo"
