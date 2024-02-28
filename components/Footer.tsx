92afced0f04e2a61d6573df8d739f355f055c044
ChiefLopes <chieflopesekede@gmail.com>
2024-02-28 04:53:35 +0100

    Done with the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,9 @@ const Footer = () => {
   return (
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
-        <div className="flex flex-col items-start justify-center gap-[10]% md:flex-row ">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image
-              src="/hilink-logo.svg"
-              alt="hilink logo"
-              width={74}
-              height={29}
-            />
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
@@ -32,15 +27,15 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((links) => (
+                {FOOTER_CONTACT_INFO.links.map((link) => (
                   <Link
                     href="/"
-                    key={links.label}
+                    key={link.label}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
-                    <p className="whitespace-nowrap">{links.label}:</p>
-                    <p className="medium-14 whitespace-nowrap  text-blue-70">
-                      {links.value}
+                    <p className="whitespace-nowrap">{link.label}:</p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
                     </p>
                   </Link>
                 ))}
@@ -60,9 +55,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
         <div className="border bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">
-          2024 Hilink | All Rigths Reserved
+          2023 Hilink | All rights reserved
         </p>
       </div>
     </footer>
